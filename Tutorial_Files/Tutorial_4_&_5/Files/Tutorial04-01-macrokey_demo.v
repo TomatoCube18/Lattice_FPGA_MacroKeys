bd27c7054468bb7e057f1fd32ad19ea858e7598f
Tomato Cube <47109215+TomatoCube18@users.noreply.github.com>
2024-08-29 09:18:32 +0800

    Add files via upload

--- Tutorial_Files/Tutorial_4_&_5/Files/Tutorial04-01-macrokey_demo.v
+++ Tutorial_Files/Tutorial_4_&_5/Files/Tutorial04-01-macrokey_demo.v
@@ -8,47 +8,47 @@ module MacroKeyDemo(swA,swB,swC,swD,swE,swF,swU,rx,tx,tx2);
   input wire swE;
   input wire swF;	
   input wire swU;
-  
+
   input wire rx;
   output wire tx;
   output wire tx2;
-    
+
   parameter SYS_FREQ = 12_090_000;
-   
+
   //Uart Debugger Output
   wire tx_wire;
   assign tx2 = tx_wire;	// Cloning the UART TX to CH9329 for External Watcher
   assign tx  = tx_wire;	
-  
+
   // Flip Flop Key
   reg key_out_ff2;
-   
+    
   // Internal OSC setting (12.09 MHz)
   OSCH #( .NOM_FREQ("12.09")) IOSC (
-        .STDBY(1'b0),
-        .OSC(clk),
-        .SEDSTDBY()
+      .STDBY(1'b0),
+      .OSC(clk),
+      .SEDSTDBY()
   );
-  
+
   // CH9329 KeyStroke Sender (UART)
   always @(posedge clk or negedge swU) begin
-        if(swU == 0)
-            key_out_ff2 <= 0;
-        else
-            key_out_ff2 <= swB;
+      if(swU == 0)
+          key_out_ff2 <= 0;
+      else
+          key_out_ff2 <= swB;
   end
   // Send Write Pulse when Falling-Edge detected on CherryMX Switch B
   assign uartStart = !swB && key_out_ff2;			
 
   ch9329_keystroke_sender #(SYS_FREQ) ch9329_keystroke_sender_u (
-    .clk	(clk	),						// System clock
-    .rst_n	(swU	),     			// Active low reset
-    .start	(uartStart	),		// Start signal to send keystroke
-    .modifier	(8'h02	),  		// Keycode modifier e.g. Shift, Alt...
-    .keycode	(8'h04	),   		// Keycode to send (HID code)
-    .autorelease	(1'b01	),	// Send a key-release after short delay
-    .tx		(tx_wire		),			// UART transmit line
-    .done	(	)            			// Transmission complete
-	);
+      .clk	(clk	),						// System clock
+      .rst_n	(swU	),     			// Active low reset
+      .start	(uartStart	),		// Start signal to send keystroke
+      .modifier	(8'h02	),  		// Keycode modifier e.g. Shift, Alt...
+      .keycode	(8'h04	),   		// Keycode to send (HID code)
+      .autorelease	(1'b01	),	// Send a key-release after short delay
+      .tx		(tx_wire		),			// UART transmit line
+      .done	(	)            			// Transmission complete
+  );
  
 endmodule
